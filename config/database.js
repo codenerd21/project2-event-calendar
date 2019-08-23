c18a68d2caa64627fbbcfebb86e392948576f819
Braxton Toomer <braxton.toomer@gmail.com>
2019-08-23 14:25:26 -0700

    added user file for models, database setup and dotenv config

--- config/database.js
+++ config/database.js
@@ -1,3 +1,11 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb:')
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+});
+
+mongoose.connection.on('connected', function() {
+  console.log(`Connected to MongoDB at ${process.env.DATABASE_URL}`);
+});
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//configures dotenv
+require('dotenv').config();
+//require the database config file (connect to DB)
+require('./config/database');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
