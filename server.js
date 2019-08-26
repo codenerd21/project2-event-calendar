cbea48d8f0c20fc281b809ebc8172dade21b6ac8
Braxton Toomer <braxton.toomer@gmail.com>
2019-08-25 22:51:18 -0700

    adds bootstrap calendar modules and configures events router

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 require('./config/database');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var eventsRouter = require('./routes/events');
 
 var app = express();
 
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/events', eventsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
