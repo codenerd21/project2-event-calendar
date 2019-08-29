2131b27bb095b4576821c74e84b7d3674a0b5485
Braxton Toomer <braxton.toomer@gmail.com>
2019-08-29 08:05:40 -0700

    adds css to show page to view info

--- models/event.js
+++ models/event.js
@@ -25,7 +25,15 @@ const eventSchema = new Schema({
     type: String,
     required: true
   },
-  address: {
+  street: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  state: {
     type: String,
     required: true
   },
